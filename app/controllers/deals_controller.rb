d9495ff72ff05ee3cfa98c06b1dbe3576f7f28b2
Steven Tegnelia <stegnelia@gmail.com>
2019-07-02 19:36:57 +0900

    deal controller, new deal

---
+++ app/controllers/deals_controller.rb
@@ -0,0 +1,21 @@
+class DealsController < ApplicationController
+  def new
+    @item = Item.find(params[:id])
+    @deal = Deal.new
+  end
+
+  def create
+    @deal = Deal.new(deal_params)
+    @deal.user = User.find(params[:user_id])
+    @deal.item = Item.find(params[:item][:item_id])
+    @deal.start_date = Date.today
+    @deal.save
+    redirect_to listings_path
+  end
+
+  private
+
+  def deal_params
+    params.require(:deal).permit(:duration)
+  end
+end
