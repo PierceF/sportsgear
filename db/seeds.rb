616d465db0f975f63163593f9e8b7b90c9afc318
Pontus Berg <pontusberg95@gmail.com>
2019-07-02 14:14:01 +0900

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
-=======
 skates = [
   {
     name: 'skates',
@@ -15,6 +13,3 @@ skates = [
   }
 ]
 Item.create!(skates)
-
->>>>>>> 07d93789fe6bdefe25d6a641df5b23a5ae8b007b
-
