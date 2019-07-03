9a7945bbf95285bcc81cad7a5105491f39c26c93
Pierce Franklin <piush_r@hotmail.com>
2019-07-03 13:13:27 +0900

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,6 +33,25 @@ ball = [
   }
 ]
 
+tent = [
+  {
+    name: 'Tent',
+    description: 'tent',
+    price: 18,
+    owner_id: 2
+  }
+]
+
+shoes = [
+  {
+    name: 'shoes',
+    description: 'roll around',
+    price: 18,
+    owner_id: 2
+  }
+]
+
+
 deal1 = [
   {
     status: 'penng',
@@ -40,8 +59,11 @@ deal1 = [
     user_id: 1
   }
 ]
+
 Item.create!(skates)
 Item.create!(ball)
 Deal.create!(deal1)
+Item.create!(tent)
+Item.create!(shoes)
 
 
