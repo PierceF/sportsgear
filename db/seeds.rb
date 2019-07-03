36c8bbe5d13ff70c712e7f0db00919527625de24
Pontus Berg <pontusberg95@gmail.com>
2019-07-03 14:01:18 +0900

    fixed floating button in the listings page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-skates = [
-  {
-    name: 'skates',
-    description: 'roll around',
-    price: 18,
-    owner_id: 1
-  }
-]
 
-ball = [
-  {
-    name: 'ball',
-    description: 'roll around',
-    price: 18,
-    owner_id: 2
-  }
-]
-
-deal1 = [
-  {
-    status: 'pending',
-    item_id: 1,
-    user_id: 1
-  }
-]
 Item.create!(skates)
 Item.create!(ball)
 Deal.create!(deal1)
