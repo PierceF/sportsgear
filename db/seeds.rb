540aa4da4e0acfd46137e4df5385c3ba70916c94
Steven Tegnelia <stegnelia@gmail.com>
2019-07-04 10:56:56 +0900

    seeds and removed gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ steven = Owner.create!(email: 'steven@steven.com',password: '123456')
 pierce = User.create!(email: 'pierce@pierce.com',password: '123456')
 
 
-
+skates = Item.create!(name: 'skates', price: 12, description: 'Used to roll around', owner_id: 1)
+football = Item.create!(name: 'football', price: 15, description: 'An American handegg', owner_id: 2)
 
 
