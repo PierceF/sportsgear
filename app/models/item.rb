ea2cfb1d17fcacbf23f4689c9ce8d9ae009360ce
Pierce Franklin <piush_r@hotmail.com>
2019-07-04 14:21:32 +0900

    added keyword searching

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,14 @@ class Item < ApplicationRecord
   belongs_to :owner
   has_many :deals
   mount_uploader :photos, PhotoUploader
+  include PgSearch
+  # Movie.search_by_title_and_syllabus(query)
+  pg_search_scope :search_by_name_and_description,
+    against: {
+      name: 'A',
+      description: 'B'
+    },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
