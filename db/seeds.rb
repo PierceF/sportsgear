79704ca49765456ef7df7e6e31a98d3ddfafeffa
Steven Tegnelia <stegnelia@gmail.com>
2019-07-05 13:55:56 +0900

    added stuff for user page

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,10 +19,10 @@ drone = Item.create!(name: 'drone', price: 35, description: 'Annoy everyone with
 rentedItem2 = Item.create!(name: 'rented out', price: 15, description: 'This item has been rented', owner_id: 1)
 
 deal1 = Deal.create!(item_id: 1, user_id: 1, status: 'pending', duration: 2, start_date: Date.today, end_date: Date.today + 2)
-deal2 = Deal.create!(item_id: 2, user_id: 1, status: 'pending', duration: 2, start_date: Date.today, end_date: Date.today + 2)
+deal2 = Deal.create!(item_id: 2, user_id: 2, status: 'pending', duration: 2, start_date: Date.today, end_date: Date.today + 2)
 deal3 = Deal.create!(item_id: 3, user_id: 1, status: 'ongoing', duration: 2, start_date: Date.today, end_date: Date.today + 2)
 deal4 = Deal.create!(item_id: 3, user_id: 1, status: 'finished', duration: 2, start_date: Date.today - 10, end_date: Date.today - 8)
-deal5 = Deal.create!(item_id: 3, user_id: 1, status: 'ongoing', duration: 2, start_date: Date.today - 2, end_date: Date.today - 1)
+deal5 = Deal.create!(item_id: 5, user_id: 2, status: 'ongoing', duration: 2, start_date: Date.today - 2, end_date: Date.today - 1)
 
 deal6 = Deal.create!(item_id: 6, user_id: 1, status: 'ongoing', duration: 2, start_date: Date.today, end_date: Date.today + 2)
 deal7 = Deal.create!(item_id: 1, user_id: 1, status: 'finished', duration: 2, start_date: Date.today - 10, end_date: Date.today - 8)
